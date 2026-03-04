d2448c41304de0eca4997c05c944540a64327dc3
João Pedro Albino <joaopedroalbino05@gmail.com>
2026-03-04 16:23:39 -0300

    Update

--- TrabalhoLustreComVentilador/src/br/com/fiapride/main/SistemaPrincipal.java
+++ TrabalhoLustreComVentilador/src/br/com/fiapride/main/SistemaPrincipal.java
@@ -12,18 +12,18 @@ public class SistemaPrincipal {
         LustreComVentilador lustre2 =
                 new LustreComVentilador("1 hélices e 0 lâmpada", "Pequeno", "Madeira");
 
-        // ESTADO ORIGINAL
+        
         System.out.println("===== ESTADO ORIGINAL =====");
         lustre1.exibirInformacoes("Lustre 1");
         lustre2.exibirInformacoes("Lustre 2");
 
-        // ALTERANDO APENAS O LUSTRE 2 (válido)
+       
         lustre2.alterarEstrutura("3 hélices e 1 lâmpada");
 
-        // TESTE INVÁLIDO (não deve alterar nada)
-        lustre2.alterarEstrutura(""); // inválido (não muda estado)
+      
+        lustre2.alterarEstrutura(""); 
 
-        // ESTADO APÓS ALTERAÇÃO
+     
         System.out.println("===== APÓS ALTERAÇÃO DO LUSTRE 2 =====");
        
         lustre1.exibirInformacoes("Lustre 1");
