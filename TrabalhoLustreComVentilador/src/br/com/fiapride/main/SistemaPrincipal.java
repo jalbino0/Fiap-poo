9bc9a9d2060ce9590d5f9f92e6b9fae3a75e0904
João Pedro Albino <joaopedroalbino05@gmail.com>
2026-04-01 20:56:09 -0300

    Update SistemaPrincipal.java

--- TrabalhoLustreComVentilador/src/br/com/fiapride/main/SistemaPrincipal.java
+++ TrabalhoLustreComVentilador/src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,34 +1,26 @@
 package br.com.fiapride.main;
 
-import br.com.fiapride.model.LustreComVentilador;
+import br.com.fiapride.model.Passageiro;
+import br.com.fiapride.model.Veiculo;
+import br.com.fiapride.model.Viagem;
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
 
-        System.out.println("--- Teste do LustreComVentilador ---\n");
+        System.out.println("--- FIAPRIDE: Inicializando Sistema ---\n");
 
-        LustreComVentilador lustre1 =
-                new LustreComVentilador("3 hélices e 2 lâmpadas", "Médio", "Plástico");
+        Passageiro ana = new Passageiro("Ana Silva", "222.222.222-22");
+        Veiculo carroDoJoao = new Veiculo("ABC-1234", "Toyota Corolla");
 
-        LustreComVentilador lustre2 =
-                new LustreComVentilador("1 hélice e 0 lâmpadas", "Pequeno", "Madeira");
+        Viagem viagemDaAna = new Viagem("Avenida Paulista, 1000", ana, carroDoJoao);
 
-        System.out.println("--- Estado Inicial ---");
-        lustre1.exibirInformacoes("Lustre 1");
-        lustre2.exibirInformacoes("Lustre 2");
+        viagemDaAna.exibirResumo();
 
-        System.out.println("--- Alterando dados ---");
-        lustre2.atualizarTamanho("Grande");
-        lustre2.atualizarMaterial("Metal");
-        lustre2.atualizarEstrutura("4 hélices e 3 lâmpadas");
+        System.out.println("--- Adicionando saldo à passageira ---");
+        ana.adicionarSaldo(50.0);
 
-        System.out.println("\n--- Testando regras de validação ---");
-        lustre2.atualizarTamanho("");
-        lustre2.atualizarMaterial(null);
-
-        System.out.println("\n--- Estado Final ---");
-        lustre1.exibirInformacoes("Lustre 1");
-        lustre2.exibirInformacoes("Lustre 2");
+        System.out.println("\nSaldo da Ana consultado ATRAVÉS da viagem: R$ "
+                + viagemDaAna.getSolicitante().getSaldo());
     }
 }
