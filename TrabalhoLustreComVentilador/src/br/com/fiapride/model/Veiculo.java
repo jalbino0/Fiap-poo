3baf43b25f55bbabcf23256cea22f246613b891d
João Pedro Albino <joaopedroalbino05@gmail.com>
2026-04-29 21:53:06 -0300

    Add fuel level management to Veiculo class

--- TrabalhoLustreComVentilador/src/br/com/fiapride/model/Veiculo.java
+++ TrabalhoLustreComVentilador/src/br/com/fiapride/model/Veiculo.java
@@ -4,10 +4,12 @@ public class Veiculo {
 
     private String placa;
     private String modelo;
+    private double nivelCombustivel;
 
     public Veiculo(String placa, String modelo) {
         this.setPlaca(placa);
         this.setModelo(modelo);
+        this.setNivelCombustivel(0);
     }
 
     public String getPlaca() {
@@ -18,8 +20,28 @@ public class Veiculo {
         return this.modelo;
     }
 
+    public double getNivelCombustivel() {
+        return this.nivelCombustivel;
+    }
+
     public void atualizarPlaca(String novaPlaca) {
+        System.out.println("Solicitada atualização de placa no Detran para o veículo "
+                + this.modelo + ".");
         this.setPlaca(novaPlaca);
+    }
+
+    public void abastecer(int quantidade) {
+        if (quantidade > 0) {
+            this.setNivelCombustivel(this.getNivelCombustivel() + quantidade);
+            System.out.println("Abastecimento realizado. Nível atual: "
+                    + this.getNivelCombustivel() + " litros.");
+        } else {
+            System.out.println("Erro: a quantidade de combustível deve ser maior que zero.");
+        }
+    }
+
+    public String calcularAutonomia() {
+        return "Autonomia não definida para um veículo genérico.";
     }
 
     private void setPlaca(String placa) {
@@ -35,6 +57,14 @@ public class Veiculo {
             this.modelo = modelo;
         } else {
             System.out.println("Erro: o modelo não pode ser vazio.");
+        }
+    }
+
+    private void setNivelCombustivel(double nivelCombustivel) {
+        if (nivelCombustivel >= 0) {
+            this.nivelCombustivel = nivelCombustivel;
+        } else {
+            System.out.println("Erro: o nível de combustível não pode ser negativo.");
         }
     }
 }
