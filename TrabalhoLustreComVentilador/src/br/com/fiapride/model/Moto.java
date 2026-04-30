68354e013831941145a2699495dcd401fee15155
João Pedro Albino <joaopedroalbino05@gmail.com>
2026-04-29 21:54:19 -0300

    Add Moto

---
+++ TrabalhoLustreComVentilador/src/br/com/fiapride/model/Moto.java
@@ -0,0 +1,21 @@
+package br.com.fiapride.model;
+
+public class Moto extends Veiculo {
+
+    private boolean isEletrica;
+
+    public Moto(String placa, String modelo, boolean isEletrica) {
+        super(placa, modelo);
+        this.isEletrica = isEletrica;
+    }
+
+    public boolean isEletrica() {
+        return this.isEletrica;
+    }
+
+    @Override
+    public String calcularAutonomia() {
+        double autonomia = this.getNivelCombustivel() * 35;
+        return "Autonomia da moto: " + autonomia + " km.";
+    }
+}
